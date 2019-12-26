7dcb7a115760b09051dc42d4ebda109886721b3a
Florin Mihalache <florinrmfm3@gmail.com>
2019-12-26 17:00:32 +0200

    Login works (no stored procedure for now)

--- ProiectBD2/Form1.Designer.cs
+++ ProiectBD2/Form1.Designer.cs
@@ -29,13 +29,14 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(480, 80);
+            this.label1.Location = new System.Drawing.Point(230, 56);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(219, 49);
             this.label1.TabIndex = 0;
@@ -46,7 +47,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(480, 252);
+            this.label2.Location = new System.Drawing.Point(230, 228);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(204, 49);
             this.label2.TabIndex = 1;
@@ -55,7 +56,7 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox1.Location = new System.Drawing.Point(489, 162);
+            this.textBox1.Location = new System.Drawing.Point(239, 138);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(313, 57);
             this.textBox1.TabIndex = 2;
@@ -63,7 +64,7 @@
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox2.Location = new System.Drawing.Point(489, 332);
+            this.textBox2.Location = new System.Drawing.Point(239, 308);
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(313, 57);
@@ -72,28 +73,40 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(489, 443);
+            this.button1.Location = new System.Drawing.Point(239, 419);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(313, 63);
             this.button1.TabIndex = 4;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button2.Location = new System.Drawing.Point(489, 535);
+            this.button2.Location = new System.Drawing.Point(239, 511);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(313, 89);
             this.button2.TabIndex = 5;
             this.button2.Text = "Create new account";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(235, 379);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1205, 673);
+            this.ClientSize = new System.Drawing.Size(759, 673);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
@@ -116,6 +129,7 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- ProiectBD2/Form1.cs
+++ ProiectBD2/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,10 +16,52 @@ namespace ProiectBD2 {
         }
 
         private void Form1_Load(object sender, EventArgs e) {
-
+            
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool doesExist = false;
+            // check if user exists in data base
+            using (var conn =
+                new SqlConnection("Data Source=(local);Initial Catalog=BD2;Integrated Security=SSPI"))
+            {
+
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("select * from Users", conn))
+                {
+
+                    SqlDataReader rdr = null;
+                    rdr = cmd.ExecuteReader();
+
+                    if (rdr.Read())
+                    {
+                        var user = rdr[0].ToString();
+                        var pass = rdr[1].ToString();
+
+                        if (user.Equals(textBox1.Text) && pass.Equals(textBox2.Text))
+                        {
+                            doesExist = true;
+                        }
+                    }
+                }
+            }
+
+            if (doesExist)
+            {
+                new Form2().Show();
+            } else
+            {
+                label3.Text = "Invalid credentials";
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
         {
 
         }
